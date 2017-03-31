783bf96c6ab4f5c2e20ace2bcb237854956f1493
Yves Renaud <ys.renaud@gmail.com>
2017-04-01 00:48:57 +0200

    chore: enable dev tools extension

--- app/stores/configureStore.js
+++ app/stores/configureStore.js
@@ -4,6 +4,7 @@ import reducer from '../reducers/index'
 
 const store = compose(
     applyMiddleware(thunk),
+    window.devToolsExtension ? window.devToolsExtension() : f => f
 )(createStore)(reducer)
 
 export default store
