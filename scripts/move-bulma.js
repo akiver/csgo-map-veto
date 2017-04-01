4485ef1f62cf8d7f20c901e81a9629200fa1e15d
AkiVer <ys.renaud@gmail.com>
2017-04-01 14:35:02 +0200

    chore: fix bulma script on windows

--- scripts/move-bulma.js
+++ scripts/move-bulma.js
@@ -4,11 +4,11 @@
  * As a result I have to copy / paste the bulma lib at the same level than the app.sass file.
  * This is script is launched at npm prepublish event.
  */
-const shell = require('child_process').execSync
-const bulmaSrc = `${__dirname}/../node_modules/bulma/`
-const bulmaDest = `${__dirname}/../app/bulma`
+const shell = require('shelljs')
+const path = require('path')
+const bulmaSrc = path.join(__dirname,  '..', 'node_modules', 'bulma')
+const bulmaDest = path.join(__dirname, '..', 'app', 'bulma')
 
-shell(`mkdir -p ${bulmaDest}`)
-shell(`cp -r ${bulmaSrc}/* ${bulmaDest}`)
+shell.cp('-R', bulmaSrc, bulmaDest)
 
 console.log('bulma has been copied to', bulmaDest)
